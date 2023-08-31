533e0f236d28e3e5258a16aba536265668506be1
seonyo <jsy6535@naver.com>
2023-08-31 13:57:23 +0900

    :sparkle:[Feat] 게임준비화면 시작버튼과 제시어 버튼 추가

--- src/main/java/org/zero/frame/GameReady.java
+++ src/main/java/org/zero/frame/GameReady.java
@@ -13,7 +13,8 @@ public class GameReady extends JFrame{
 	public GameReady() {
 		// 시작기본세팅 메서드
 		CommonUtil.settings(this);
-
+		String arrBtn[] = {"동물", "음식", "사물", "장소"};
+		int btnX = 155;
 		//배경 패널 생성
 		JPanel p = new JPanel() {
 			@Override
@@ -41,6 +42,25 @@ public class GameReady extends JFrame{
 		readyText.setFont(midFont);
 		readyText.setForeground(new Color(142,110,0));
 		p.add(readyText);
+
+		for(int i=0; i<arrBtn.length; i++){
+			JButton btn = new JButton(arrBtn[i]);
+			btn.setBounds(btnX, 270, 100, 35);
+			btnX += 115;
+			btn.setBackground(new Color(255,255,255));
+			btn.setFont(midFont);
+			btn.setForeground(new Color (142,110,0));
+			p.add(btn);
+		}
+
+		JButton gamestartBtn = new JButton("게임시작");
+		gamestartBtn.setBounds(500,383, 170, 45);
+		gamestartBtn.setBackground(new Color(255,228,131));
+		gamestartBtn.setBorder(new RoundBorder(30, new Color(255, 228, 131), 2.0f));
+		gamestartBtn.setForeground(new Color(168, 131, 0));
+		gamestartBtn.setFont(midFont);
+		p.add(gamestartBtn);
+
 		setVisible(true);
 
 
