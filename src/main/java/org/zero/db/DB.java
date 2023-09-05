347b46adf54edce96fa307b36cdbce82e536f522
haeunNoh06 <okhagrace06@naver.com>
2023-09-05 14:40:25 +0900

    :card_file_box: [refactor] db 연동 오류 해결
    - test 값 넣어보기

--- src/main/java/org/zero/db/DB.java
+++ src/main/java/org/zero/db/DB.java
@@ -2,7 +2,7 @@ package org.zero.db;
 
 public class DB {
 	public static class MySQL {
-		public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+//		public static final String DRIVER_NAME = "com.mysql.jdbc.driver";
 		// 하은 학교 서버
 		public static final String JDBC_URL = "jdbc:mysql://10.96.122.57:3306/catchmind_db?user=root&password=gkdms~!1357";
 		// 하은 개인 노트북 서버
