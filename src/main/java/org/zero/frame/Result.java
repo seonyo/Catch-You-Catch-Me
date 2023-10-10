4d38087c2fee87497cb7add09b0c8d40bdf60cee
haeunNoh06 <okhagrace06@naver.com>
2023-10-11 08:16:16 +0900

    :lipstick: [design] 등수, 팀원, 플레이타임 적용
    - 배경 이미지 삽입 필요

--- src/main/java/org/zero/frame/Result.java
+++ src/main/java/org/zero/frame/Result.java
@@ -6,15 +6,23 @@ import org.zero.common.CommonUtil;
 import javax.swing.*;
 import java.awt.*;
 
+import static javax.swing.BorderFactory.createEmptyBorder;
 import static org.zero.common.CommonUtil.*;
 
 public class Result extends JFrame{
 	// 선언 및 생성
+	private JPanel backgroundPanel;// 백그라운드 배경 넣는 패널
+	private JPanel titlePanel = new JPanel(new BorderLayout());// 제목 넣는 패널
+	private JPanel rankBackgroundPanel = new JPanel(null);// 랭킹 전체를 감싸는 패널
+	private JPanel[] rankPanel;// 하나의 랭킹을 감싸는 패널
+	private JPanel playersPanel = new JPanel();
 	private Image background = new ImageIcon(Main.class.getResource("/static/img/result.png")).getImage();
 	private ImageIcon preBtnImg = new ImageIcon(Main.class.getResource("/static/img/beforeBtn.png"));
+	private ImageIcon rankBackgroundImg = new ImageIcon(Main.class.getResource("/static/img/rank_background.png"));
+	private JLabel rankBackImgLabel = new JLabel(rankBackgroundImg);// 랭킹 배경 이미지
 	private JLabel title = new JLabel("Result");
-	private JLabel backgroundLabel = new JLabel(new ImageIcon(background));
-	private JPanel rankPanel = new JPanel(new BorderLayout());
+	private JLabel rank;// 등수
+	JLabel[][] players = new JLabel[10][4];// 플레이어 명단
 	private JScrollPane scroll = new JScrollPane();
 	private JButton beforeBtn = new JButton();
 
@@ -22,41 +30,76 @@ public class Result extends JFrame{
 		// 시작기본세팅 메서드
 		settings(this);
 
-		// 제목 스타일
+		// 랭킹 바탕 그림
+		backgroundPanel = new JPanel(null) {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				g.drawImage(background, 0, 0, null);
+			}
+		};
+		backgroundPanel.setBounds(0,0,CommonUtil.WIDTH,CommonUtil.HEIGHT);
+		add(backgroundPanel);
+
+		// 제목
 		title.setFont(CommonUtil.titleFont);
 		title.setForeground(titleColor);
+		titlePanel.setSize(330, 55);
+		titlePanel.setBackground(new Color(0,0,0,0));
+		titlePanel.setBounds(330, 55, 120, 120);
+		titlePanel.add(title);
 
 		// 버튼 스타일
 		beforeBtn.setIcon(preBtnImg);
 		beforeBtn.setBorderPainted(false); // 버튼 테두리 설정해제
 		beforeBtn.setFocusPainted(false);
 		beforeBtn.setBackground(Color.WHITE);
+		beforeBtn.setBounds(40,395,50,50);
 
-		JLabel ranking1 = new JLabel("1");
-		ranking1.setBounds(125,170, 20,20);
-		ranking1.setFont(semiMidFont);
-		this.add(ranking1);
+		// 스크롤
+//		JScrollPane sp = new JScrollPane(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+//		sp.setBounds(100,0, CommonUtil.WIDTH-213,300);
+//		sp.add(rankPanel);
+//		sp.setOpaque(false);// 배경색 투명하게
+//		sp.getViewport().setOpaque(false);// 배경색 투명하게
+//		sp.setBorder(createEmptyBorder());// 외곽선 투명하게
+//		rankBackgroundPanel.add(sp);
 
-		// 플레이어 명단
-		JLabel []players = new JLabel[4];
-		for (int i =  0; i < players.length; i++) {
-			players[i] = new JLabel("노하은");
-			players[i].setFont(semiMidFont);
-			players[i].setBounds(100+((i+1)*80), 170, 120,20);
-			this.add(players[i]);
+		// 랭킹
+		rankBackgroundPanel.setBounds(0,140, CommonUtil.WIDTH,300);
+		//rankBackgroundPanel.setBackground(Color.BLUE);
+		rankBackgroundPanel.setBackground(new Color(0,0,0,0));
+		rankPanel = new JPanel[players.length];
+		for ( int i = 0; i < players.length; i++) {
+			rankPanel[i] = new JPanel(null);
+			// 등수
+			rank = new JLabel((i+1)+"");
+			rank.setBounds(30,10, 20,22);
+			rank.setFont(semiMidFont);
+			rankPanel[i].add(rank);
+
+			// 랭킹 배경 이미지
+			rankBackImgLabel.setBounds(-15, 10, CommonUtil.WIDTH, 45);
+			rankPanel[i].add(rankBackImgLabel);
+
+			// 플레이타임
+			JLabel time = new JLabel("30:30");
+			time.setFont(semiLargeFont);
+			time.setForeground(titleColor);
+			time.setBounds(440, -40, 120, 120);
+			rankPanel[i].add(time);
+
+			// 팀원 명단
+			for (int j =  0; j < players[i].length; j++) {
+				players[i][j] = new JLabel("노하은");
+				players[i][j].setFont(semiMidFont);
+				players[i][j].setBounds((80 + j * 80), 12, 120, 20);
+				rankPanel[i].add(players[i][j]);
+			}
+
+			rankPanel[i].setBounds(100, 10+i*60, CommonUtil.WIDTH-230, 45);
+			rankBackgroundPanel.add(rankPanel[i]);
 		}
-		JLabel time = new JLabel("30:30");
-		time.setFont(semiLargeFont);
-		time.setForeground(titleColor);
-		time.setBounds(530, 120, 120, 120);
-		this.add(time);
-
-		// 랭킹 바탕 그림
-		JLabel rankBackground = new JLabel();
-		ImageIcon rankBackgroundImg = new ImageIcon(Main.class.getResource("/static/img/rank_background.png"));
-		rankBackground.setIcon(rankBackgroundImg);
-		rankBackground.setBounds(100,130,600,100);
-		this.getContentPane().add(rankBackground);
 
 		// 이벤트 처리
 		beforeBtn.addActionListener( e -> {
@@ -65,18 +108,15 @@ public class Result extends JFrame{
 		});
 
 		// 좌표 설정
-		beforeBtn.setBounds(50,395,50,50);
-		title.setBounds(330, 55, 120, 120);
-		rankPanel.setBounds(150,150, 300,300);
-		scroll.setBounds(0,0, 300,300);
-		backgroundLabel.setBounds(0,0,750,500);
-		backgroundLabel.add(title);
+		//scroll.setBounds(0,0, 300,300);
 
-		this.add(backgroundLabel);
-		this.add(beforeBtn);
-		this.add(rankPanel);
+		backgroundPanel.add(rankBackgroundPanel);
+		backgroundPanel.add(titlePanel);
+		backgroundPanel.add(beforeBtn);
+
 		this.setVisible(true);
 	}
+
 
 	public static void main(String[] args) {
 		Result r = new Result();
