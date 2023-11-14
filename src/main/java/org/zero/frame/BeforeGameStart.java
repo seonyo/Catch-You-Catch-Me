bfaeb55e8ca79e9b469db92f534ff233cd285009
haeunNoh06 <okhagrace06@naver.com>
2023-11-14 15:09:07 +0900

    :sparkles: [feat] 게임 나가기 확인창 구현

--- src/main/java/org/zero/frame/BeforeGameStart.java
+++ src/main/java/org/zero/frame/BeforeGameStart.java
@@ -113,7 +113,16 @@ public class BeforeGameStart extends JFrame {
         readyBtn.setForeground(new Color(142, 110, 0));
         readyBtn.setFont(semiMidFont);
         backgroundPanel.add(readyBtn);
+
         JButton exitBtn = new JButton("나가기");
+        exitBtn.addActionListener(e -> {
+            int result = JOptionPane.showConfirmDialog(null, "정말 게임을 종료하시겠습니까?");
+            if (result == JOptionPane.YES_OPTION) {
+                this.setVisible(false);
+                new Main();
+            }
+        });
+
         exitBtn.setBounds(616, 411, 90, 30);
         exitBtn.setBackground(new Color(255, 228, 131));
         exitBtn.setForeground(new Color(142, 110, 0));
@@ -128,7 +137,7 @@ public class BeforeGameStart extends JFrame {
         chatArea.setEditable(false);
         // 자동 줄바꿈
         chatArea.setLineWrap(true);
-        JScrollPane scrollPane = new JScrollPane(chatArea,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        JScrollPane scrollPane = new JScrollPane(chatArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         chattingPn.add(scrollPane, BorderLayout.CENTER);
 
         messageField = new JTextField();
@@ -243,7 +252,7 @@ public class BeforeGameStart extends JFrame {
     public void focusRecentChat(JScrollPane scrollPane) {
         scrollPane.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener() {
             public void adjustmentValueChanged(AdjustmentEvent e) {
-                if(e.getAdjustable().getMaximum() != prevMax) {
+                if (e.getAdjustable().getMaximum() != prevMax) {
                     e.getAdjustable().setValue(e.getAdjustable().getMaximum());
                     prevMax = e.getAdjustable().getMaximum(); // 이전 최대 값을 업데이트
                 }
